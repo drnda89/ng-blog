38221475935b5cdd3f0177fd205fb779f22c3532
drnda89 <drnda89@gmail.com>
2018-11-22 16:42:29 +0100

    od kuce

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatExpansionModule, MatInputModule],
-  exports: [MatButtonModule, MatToolbarModule, MatExpansionModule, MatInputModule],
+  imports: [MatButtonModule, MatToolbarModule, MatExpansionModule, MatInputModule, MatProgressBarModule],
+  exports: [MatButtonModule, MatToolbarModule, MatExpansionModule, MatInputModule, MatProgressBarModule],
 })
 export class MaterialModule { }
--- src/app/posts/post-dashboard/post-dashboard.component.ts
+++ src/app/posts/post-dashboard/post-dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { StringFormat } from '@angular/fire/storage/interfaces';
 import { AuthService } from 'src/app/core/auth.service';
 import { PostService } from '../post.service';
+import { Observable } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -13,8 +16,11 @@ export class PostDashboardComponent implements OnInit {
   title: string;
   image: string = null;
   content: string;
+  buttonText: string = 'Create Post';
+  uploadPercent: Observable<number>;
+  downloadUrl: Observable<string>;
 
-  constructor(private auth: AuthService, private postService: PostService) { }
+  constructor(private auth: AuthService, private postService: PostService, private storage: AngularFireStorage) { }
 
   ngOnInit() {
   }
@@ -29,6 +35,34 @@ export class PostDashboardComponent implements OnInit {
       title: this.title
     };
     this.postService.create(data);
+    this.title = '';
+    this.content = '';
+    this.buttonText = 'Post Created!';
+    setTimeout(() => this.buttonText = 'Create Post', 3000);
   }
+
+  uploadImage(event) {
+    const file = event.target.files[0];
+    const filePath = `posts/${file.name}`;
+    const fileRef = this.storage.ref(filePath);
+    if (file.type.split('/')[0] !== 'image') {
+      return alert('Only Image Files!');
+    } else {
+      const task = this.storage.upload(filePath, file);
+      this.uploadPercent = task.percentageChanges();
+      task.snapshotChanges()
+        .pipe(
+          finalize(() => fileRef.getDownloadURL()
+            .subscribe(profileUrl => {
+              this.image = profileUrl
+              console.log('La URL es: ' + profileUrl);         
+            })
+          )
+        )
+        .subscribe();
+      console.log('Image Uploaded');
+    }
+  }
+  
 
 }
